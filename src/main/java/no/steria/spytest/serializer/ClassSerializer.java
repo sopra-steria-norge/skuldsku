a84033f31cacbbe1396d45a5e7251787d394172e
Anders Karlsen <karlsenanders@gmail.com>
2013-12-30 15:43:18 +0100

    Handles null

--- src/main/java/no/steria/spytest/serializer/ClassSerializer.java
+++ src/main/java/no/steria/spytest/serializer/ClassSerializer.java
@@ -2,6 +2,9 @@ package no.steria.spytest.serializer;
 
 public class ClassSerializer {
     public String asString(Object object) {
+        if (object == null) {
+            return "<null>";
+        }
         String classname = object.getClass().getName();
         return "<" + classname + ">";
     }
--- src/test/java/no/steria/spytest/serializer/SerializerTest.java
+++ src/test/java/no/steria/spytest/serializer/SerializerTest.java
@@ -1,13 +1,21 @@
 package no.steria.spytest.serializer;
 
-import org.fest.assertions.Assertions;
 import org.junit.Test;
 
+import static org.fest.assertions.Assertions.assertThat;
+
 public class SerializerTest {
+
+    private final ClassSerializer serializer = new ClassSerializer();
+
     @Test
     public void shouldReturnClassNameForEmptyClass() throws Exception {
-        ClassSerializer serializer = new ClassSerializer();
-        Assertions.assertThat(serializer.asString(new EmptyClass())).isEqualTo("<no.steria.spytest.serializer.EmptyClass>");
+        assertThat(serializer.asString(new EmptyClass())).isEqualTo("<no.steria.spytest.    serializer.EmptyClass>");
+    }
+
+    @Test
+    public void shouldHandleNull() throws Exception {
+        assertThat(serializer.asString(null)).isEqualTo("<null>");
 
     }
 }
