9fa48462f49ac959620158c0a9828682b0bb98ee
Anders Karlsen <karlsenanders@gmail.com>
2013-12-31 13:44:47 +0100

    Commit for sync

---
+++ src/test/java/no/steria/spytest/serializer/ClassWithSimpleFields.java
@@ -0,0 +1,14 @@
+package no.steria.spytest.serializer;
+
+public class ClassWithSimpleFields {
+    private String stringval;
+
+    public String getStringval() {
+        return stringval;
+    }
+
+    public ClassWithSimpleFields setStringval(String stringval) {
+        this.stringval = stringval;
+        return this;
+    }
+}
