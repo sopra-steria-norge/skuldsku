3ec24ce6e50e44df8d63095cb781f617ff865803
Anders Karlsen <karlsenanders@hotmail.com>
2014-03-25 15:22:16 +0100

    Posting http forms

--- httpcopy/src/main/java/no/steria/httpplayer/HttpPlayer.java
+++ httpcopy/src/main/java/no/steria/httpplayer/HttpPlayer.java
@@ -3,6 +3,7 @@ package no.steria.httpplayer;
 import no.steria.httpspy.ReportObject;
 
 import java.io.*;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.List;
@@ -26,16 +27,33 @@ public class HttpPlayer {
     }
 
     private void doPlayStep(ReportObject playStep) throws IOException {
-        URLConnection conn = new URL(baseUrl + playStep.getPath()).openConnection();
-        conn.setDoOutput(true);
-        try (PrintWriter printWriter = new PrintWriter(new OutputStreamWriter(conn.getOutputStream(),"utf-8"))) {
-            printWriter.append(playStep.getReadInputStream());
+
+        URL url = new URL(baseUrl + playStep.getPath());
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        String method = playStep.getMethod();
+        conn.setRequestMethod(method);
+        String readInputStream = playStep.getReadInputStream();
+        if (readInputStream != null) {
+            conn.setDoOutput(true);
+            try (PrintWriter printWriter = new PrintWriter(new OutputStreamWriter(conn.getOutputStream(), "utf-8"))) {
+                printWriter.append(readInputStream);
+            }
+        }
+
+        String parameters = playStep.getParametersRead().entrySet().stream().map(entry -> entry.getKey() + "=" + entry.getValue()).reduce((a, b) -> a + "&" + b).orElse(null);
+        if (parameters != null) {
+            conn.setDoOutput(true);
+            DataOutputStream wr = new DataOutputStream(conn.getOutputStream());
+            wr.writeBytes(parameters);
+            wr.flush();
+            wr.close();
         }
 
         try (InputStream is = conn.getInputStream()) {
             try (Reader reader = new BufferedReader(new InputStreamReader(is, "utf-8"))) {
                 //noinspection StatementWithEmptyBody
-                while (reader.read() != -1) {}
+                while (reader.read() != -1) {
+                }
             }
 
         }
--- httpcopy/src/test/java/no/steria/httpplayer/TestHttpPlayer.java
+++ httpcopy/src/test/java/no/steria/httpplayer/TestHttpPlayer.java
@@ -5,18 +5,25 @@ import no.steria.httpspy.ReportObject;
 import org.json.JSONObject;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 import java.io.*;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import static org.fest.assertions.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 public class TestHttpPlayer {
     @Test
-    public void shouldHandleBasicForms() throws Exception {
+    public void shouldHandleJSONPost() throws Exception {
         InMemoryReporter reporter = new InMemoryReporter();
         TestFilter.setReporter(reporter);
         JettyServer jettyServer = new JettyServer(0);
@@ -64,6 +71,47 @@ public class TestHttpPlayer {
 
 
     }
+
+    @Test
+    public void shouldHandleBasicForms() throws Exception {
+        InMemoryReporter reporter = new InMemoryReporter();
+        TestFilter.setReporter(reporter);
+
+        JettyServer jettyServer = new JettyServer(0);
+        jettyServer.start();
+
+
+        try {
+            int port = jettyServer.getPort();
+
+            WebDriver browser = new HtmlUnitDriver();
+            browser.get("http://localhost:" + port + "/post/more");
+            browser.findElement(By.name("firstname")).sendKeys("Darth");
+            browser.findElement(By.name("lastname")).sendKeys("Vader");
+            browser.findElement(By.name("doPerson")).click();
+
+            CallReporter callReporter = mock(CallReporter.class);
+            TestFilter.setReporter(callReporter);
+
+
+            String baseurl = "http://localhost:" + port;
+            HttpPlayer player = new HttpPlayer(baseurl);
+            player.play(reporter.getPlayBook());
+
+            ArgumentCaptor<ReportObject> captor = ArgumentCaptor.forClass(ReportObject.class);
+            verify(callReporter,times(2)).reportCall(captor.capture());
+
+            List<ReportObject> allValues = captor.getAllValues();
+            assertThat(allValues.stream().map(ro -> ro.getMethod()).collect(Collectors.toList())).containsExactly("GET","POST");
+            assertThat(allValues.get(1).getOutput()).isEqualTo("Your name is Darth Vader");
+        } finally {
+            jettyServer.stop();
+
+        }
+
+
+    }
+
 
 
     private static String toString(InputStream inputStream) throws IOException {
