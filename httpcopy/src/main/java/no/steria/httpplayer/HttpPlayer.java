184ceeae27d0786093849b99db269d0430a74780
Anders Karlsen <karlsenanders@hotmail.com>
2014-04-03 09:53:01 +0200

    Player updated

--- httpcopy/src/main/java/no/steria/httpplayer/HttpPlayer.java
+++ httpcopy/src/main/java/no/steria/httpplayer/HttpPlayer.java
@@ -7,6 +7,8 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class HttpPlayer {
     private String baseUrl;
@@ -32,6 +34,12 @@ public class HttpPlayer {
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         String method = playStep.getMethod();
         conn.setRequestMethod(method);
+        if (playStep.getHeaders() != null) {
+            Set<Map.Entry<String, List<String>>> entries = playStep.getHeaders().entrySet();
+            for (Map.Entry<String, List<String>> entry : entries) {
+                conn.setRequestProperty(entry.getKey(),entry.getValue().get(0));
+            }
+        }
         String readInputStream = playStep.getReadInputStream();
         if (readInputStream != null) {
             conn.setDoOutput(true);
@@ -49,13 +57,17 @@ public class HttpPlayer {
             wr.close();
         }
 
+        StringBuilder result = new StringBuilder();
         try (InputStream is = conn.getInputStream()) {
             try (Reader reader = new BufferedReader(new InputStreamReader(is, "utf-8"))) {
-                //noinspection StatementWithEmptyBody
-                while (reader.read() != -1) {
+                int c;
+                while ((c = reader.read()) != -1) {
+                    result.append((char)c);
                 }
             }
 
         }
+        System.out.println("Step***");
+        System.out.println(result);
     }
 }
--- httpcopy/src/test/java/no/steria/httpplayer/fileplayback/RunPlayback.java
+++ httpcopy/src/test/java/no/steria/httpplayer/fileplayback/RunPlayback.java
@@ -1,5 +1,6 @@
 package no.steria.httpplayer.fileplayback;
 
+import no.steria.httpplayer.HttpPlayer;
 import no.steria.httpspy.ReportObject;
 import no.steria.reporter.FileCallReporter;
 
@@ -7,10 +8,12 @@ import java.util.List;
 
 public class RunPlayback {
     public static void main(String[] args) throws Exception {
-        List<ReportObject> reportObjects = FileCallReporter.readReportedObjects("/tmp/copytrack.txt");
+        List<ReportObject> reportObjects = FileCallReporter.readReportedObjects("/tmp/trcpy.txt");
 
         System.out.println("Read");
 
+        HttpPlayer httpPlayer = new HttpPlayer("http://localhost:21090/someother");
+        httpPlayer.play(reportObjects);
     }
 
 
