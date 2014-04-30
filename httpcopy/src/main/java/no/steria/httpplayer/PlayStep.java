d02a1b54d13aaa98f08be0bf9001d5986d542cb3
Anders Karlsen <karlsenanders@gmail.com>
2014-04-30 09:56:58 +0200

    Removed unused code

--- httpcopy/src/main/java/no/steria/httpplayer/PlayStep.java
+++ httpcopy/src/main/java/no/steria/httpplayer/PlayStep.java
@@ -1,16 +1,7 @@
 package no.steria.httpplayer;
 
 import no.steria.httpspy.ReportObject;
-import org.xml.sax.SAXException;
-import org.xml.sax.SAXParseException;
-import org.xml.sax.XMLReader;
-import org.xml.sax.helpers.DefaultHandler;
 
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.parsers.SAXParser;
-import javax.xml.parsers.SAXParserFactory;
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -122,75 +113,13 @@ public class PlayStep {
         return attributes;
     }
 
-    protected String replacementx() {
-        SAXParserFactory factory = SAXParserFactory.newInstance();
-        factory.setValidating(false);
-        factory.setNamespaceAware(false);
-
-        SAXParser saxParser;
-        try {
-            saxParser = factory.newSAXParser();
-            final XMLReader parser = saxParser.getXMLReader();
-
-            // Ignore the DTD declaration
-            parser.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
-            parser.setFeature("http://xml.org/sax/features/validation", false);
-        } catch (ParserConfigurationException | SAXException e) {
-            throw new RuntimeException(e);
-        }
-        ByteArrayInputStream is = new ByteArrayInputStream(recorded.getBytes());
-
-        Handler dh = new Handler(recordField);
-        try {
-            saxParser.parse(is,dh);
-
-        } catch (ParsingAborted e) {
-           return dh.getValue();
-        } catch (SAXException | IOException e) {
-            throw new RuntimeException(e);
-        }
-        return null;
-    }
 
     public void record(String output) {
         this.recorded = output;
     }
 
 
-    private static class ParsingAborted extends SAXException {
 
-    }
-
-    private static class Handler extends DefaultHandler {
-        private String key;
-        private String value;
-
-        private Handler(String key) {
-            this.key = key;
-        }
-
-        @Override
-        public void startElement(String uri, String localName, String qName, org.xml.sax.Attributes attributes) throws SAXException {
-            if (!"input".equals(qName)) {
-                return;
-            }
-            String name = attributes.getValue("name");
-            if (!key.equals(name)) {
-                return;
-            }
-            value = attributes.getValue("value");
-            throw new ParsingAborted();
-        }
-
-        public String getValue() {
-            return value;
-        }
-
-        @Override
-        public void fatalError(SAXParseException e) throws SAXException {
-
-        }
-    }
 
     public String getRecorded() {
         return recorded;
