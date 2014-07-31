97295e24ace213c4320f1e9dced4de3e3fed4733
Anders Karlsen <karlsenanders@gmail.com>
2014-07-31 15:00:16 +0200

    Started file recorder

---
+++ examples/basicServlet/src/test/java/no/steria/skuldsku/example/basicservlet/RunPlayback.java
@@ -0,0 +1,8 @@
+package no.steria.skuldsku.example.basicservlet;
+
+public class RunPlayback {
+
+    public static void main(String[] args) throws Exception {
+
+    }
+}
--- recorder/src/main/java/no/steria/skuldsku/recorder/javainterfacerecorder/interfacerecorder/MockFromServiceImpl.java
+++ recorder/src/main/java/no/steria/skuldsku/recorder/javainterfacerecorder/interfacerecorder/MockFromServiceImpl.java
@@ -7,17 +7,17 @@ import java.lang.reflect.Method;
  * This implementation of MockInterface delegates the handling of recording of interaction data to
  * the mock service provided in the constructor.
  */
-public class MockFromServiceImpl implements MockInterface {
+public class MockFromServiceImpl<T> implements MockInterface {
 
-    private final Object mockService;
+    private final T mockService;
 
-    public MockFromServiceImpl(Object mockService) {
+    private MockFromServiceImpl(T mockService) {
 
         this.mockService = mockService;
     }
 
     public static <T> MockInterface create(Class<T> interfaceClass,T mockService) {
-        return new MockFromServiceImpl(mockService);
+        return new MockFromServiceImpl<>(mockService);
     }
 
     @Override
