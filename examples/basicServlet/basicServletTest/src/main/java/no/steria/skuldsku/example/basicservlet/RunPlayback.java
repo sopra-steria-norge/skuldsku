7264d5686a51cb85853487d8a3fadeb1f0985fde
Anders Karlsen <karlsenanders@gmail.com>
2014-08-30 20:08:31 +0200

    Fixed compiler errrors

--- examples/basicServlet/basicServletTest/src/main/java/no/steria/skuldsku/example/basicservlet/RunPlayback.java
+++ examples/basicServlet/basicServletTest/src/main/java/no/steria/skuldsku/example/basicservlet/RunPlayback.java
@@ -1,11 +1,10 @@
 package no.steria.skuldsku.example.basicservlet;
 
-import no.steria.skuldsku.testrunner.HttpTestRunner;
+import no.steria.skuldsku.testrunner.httprunner.HttpPlayer;
 
 public class RunPlayback {
 
     public static void main(String[] args) throws Exception {
-        final HttpTestRunner testRunner = new HttpTestRunner("http://localhost:8081", "/tmp/runit.txt");
-        testRunner.execute();
+        final HttpPlayer testRunner = new HttpPlayer("http://localhost:8081");
     }
 }
