13e1d954d18a13b8f537709c2330427b2a8d20c1
Anders Karlsen <karlsenanders@gmail.com>
2014-09-04 22:25:17 +0200

    Returning compare result

---
+++ skuldsku-testrunner/src/main/java/no/steria/skuldsku/testrunner/interfacerunner/CompareResult.java
@@ -0,0 +1,4 @@
+package no.steria.skuldsku.testrunner.interfacerunner;
+
+public class CompareResult {
+}
--- skuldsku-testrunner/src/main/java/no/steria/skuldsku/testrunner/interfacerunner/InterfaceRecordingChecker.java
+++ skuldsku-testrunner/src/main/java/no/steria/skuldsku/testrunner/interfacerunner/InterfaceRecordingChecker.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class InterfaceRecordingChecker {
 
-    public boolean compare(List<RecordedInterfaceCall> expected, List<RecordedInterfaceCall> actual) {
-        return false;
+    public CompareResult compare(List<RecordedInterfaceCall> expected, List<RecordedInterfaceCall> actual) {
+        return null;
     }
 }
