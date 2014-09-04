e423991bc02b2dbca3a2723e50f417bfa68a1613
Anders Karlsen <karlsenanders@gmail.com>
2014-09-04 22:15:39 +0200

    InterfaceRecorderChecker shell implementation

--- skuldsku-prod/src/main/java/no/steria/skuldsku/recorder/javainterfacerecorder/interfacerecorder/ReportCallback.java
+++ skuldsku-prod/src/main/java/no/steria/skuldsku/recorder/javainterfacerecorder/interfacerecorder/ReportCallback.java
@@ -2,4 +2,5 @@ package no.steria.skuldsku.recorder.javainterfacerecorder.interfacerecorder;
 
 public interface ReportCallback {
     public void event(String className, String methodname, String parameters,String result);
+
 }
---
+++ skuldsku-testrunner/src/main/java/no/steria/skuldsku/testrunner/interfacerunner/InterfaceRecordingChecker.java
@@ -0,0 +1,10 @@
+package no.steria.skuldsku.testrunner.interfacerunner;
+
+import java.util.List;
+
+public class InterfaceRecordingChecker {
+
+    public boolean compare(List<RecordedInterfaceCall> expected, List<RecordedInterfaceCall> actual) {
+        return false;
+    }
+}
