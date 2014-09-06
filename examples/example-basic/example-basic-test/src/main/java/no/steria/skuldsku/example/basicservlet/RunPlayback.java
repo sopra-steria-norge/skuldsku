250d70727a281c761cfb0ce04cfa4f2ee722c696
Stian Grenborgen <sgrenbor@steria.no>
2014-09-06 16:42:22 +0200

    Adding playback code.

--- examples/example-basic/example-basic-test/src/main/java/no/steria/skuldsku/example/basicservlet/RunPlayback.java
+++ examples/example-basic/example-basic-test/src/main/java/no/steria/skuldsku/example/basicservlet/RunPlayback.java
@@ -1,10 +1,16 @@
 package no.steria.skuldsku.example.basicservlet;
 
+import java.util.List;
+
+import no.steria.skuldsku.recorder.httprecorder.HttpCall;
+import no.steria.skuldsku.recorder.httprecorder.reporter.FileHttpCallPersister;
 import no.steria.skuldsku.testrunner.httprunner.HttpPlayer;
 
 public class RunPlayback {
 
     public static void main(String[] args) throws Exception {
         final HttpPlayer testRunner = new HttpPlayer("http://localhost:8081");
+        final List<HttpCall> httpCalls = FileHttpCallPersister.readReportedObjects("data.txt");
+        testRunner.play(httpCalls);
     }
 }
