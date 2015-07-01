9fa486156c9e0c142210c7ddb2ab870c1993565c
ingaholen <ikh@steria.no>
2015-07-01 09:51:16 +0200

    Bug 6167 - Automatisert regresjonstest Wimpel * Ups, forgot one file...

--- skuldsku-prod/src/test/java/no/steria/skuldsku/recorder/SkuldskuNotInitializedTest.java
+++ skuldsku-prod/src/test/java/no/steria/skuldsku/recorder/SkuldskuNotInitializedTest.java
@@ -1,23 +1,39 @@
 package no.steria.skuldsku.recorder;
 
+import no.steria.skuldsku.recorder.logging.RecorderLog;
+import no.steria.skuldsku.recorder.logging.RecorderLogger;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.verify;
 
 public class SkuldskuNotInitializedTest {
 
+    @Mock
+    RecorderLogger recorderLogger;
+
     @Before
     public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        RecorderLog.setRecorderLogger(recorderLogger);
         SkuldskuAccessor.reset();
     }
     
-    @Test(expected=IllegalStateException.class)
-    public void startFailWhenNotInitialized() {
+    @Test
+    public void startLogsErrorWhenNotInitialized() {
         Skuldsku.start();
+        verify(recorderLogger, atLeastOnce()).error(anyString(), any(Throwable.class));
     }
     
-    @Test(expected=IllegalStateException.class)
-    public void stopFailWhenNotInitialized() {
+    @Test
+    public void stopLogsErrorInitialized() {
         Skuldsku.stop();
+        verify(recorderLogger, atLeastOnce()).error(anyString(), any(Throwable.class));
     }
     
     @Test
